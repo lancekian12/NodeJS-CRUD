d50f0d6245b2bcfd679d0e8fa7d09609adaccb78
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-08-18 13:05:56 +0800

    Wrap all controller with catchasync creating only one catch

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -4,121 +4,86 @@ const AppError = require('./../utils/appError');
 
 // Application Logic and HTTP Request
 
-exports.getAllStudent = async (req, res) => {
-  try {
-    //BUILD THE QUERY
-    // 1.) Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['studentName'];
-    console.log(queryObj);
-    excludedFields.forEach((el) => delete queryObj[el]);
+exports.getAllStudent = catchAsync(async (req, res, next) => {
+  //BUILD THE QUERY
+  // 1.) Filtering
+  const queryObj = { ...req.query };
+  const excludedFields = ['studentName'];
+  console.log(queryObj);
+  excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2.) Advance Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
+  // 2.) Advance Filtering
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  console.log(JSON.parse(queryStr));
 
-    let query = Student.find(JSON.parse(queryStr));
+  let query = Student.find(JSON.parse(queryStr));
 
-    // 3.) Sorting
-    if (req.query.sort) {
-      query = query.sort(req.query.sort);
-    }
-
-    // // 4.) Limiting Fields
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(',').join(' ');
-    //   query = query.select(fields);
-    // } else {
-    //   query = query.select('-__v');
-    // }
-
-    // Executing the query
-    const student = await query;
-
-    // Send Response
-    res.status(200).json({
-      status: 'success',
-      results: student.length,
-      data: {
-        student,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
+  // 3.) Sorting
+  if (req.query.sort) {
+    query = query.sort(req.query.sort);
   }
-};
 
-exports.getStudent = async (req, res) => {
-  try {
-    const student = await Student.findById(req.params.id);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        student,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  // // 4.) Limiting Fields
+  // if (req.query.fields) {
+  //   const fields = req.query.fields.split(',').join(' ');
+  //   query = query.select(fields);
+  // } else {
+  //   query = query.select('-__v');
+  // }
 
-exports.createStudent = async (req, res) => {
-  try {
-    const newStudent = await Student.create(req.body);
+  // Executing the query
+  const student = await query;
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        student: newStudent,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data sent!',
-    });
-  }
-};
+  // Send Response
+  res.status(200).json({
+    status: 'success',
+    results: student.length,
+    data: {
+      student,
+    },
+  });
+});
 
-exports.updateStudent = async (req, res) => {
-  try {
-    const student = await Student.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: 'success',
-      data: {
-        student: student,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data sent!',
-    });
-  }
-};
+exports.getStudent = catchAsync(async (req, res, next) => {
+  const student = await Student.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      student,
+    },
+  });
+});
 
-exports.deleteStudent = async (req, res) => {
-  try {
-    await Student.findByIdAndDelete(req.params.id);
+exports.createStudent = catchAsync(async (req, res, next) => {
+  const newStudent = await Student.create(req.body);
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    data: {
+      student: newStudent,
+    },
+  });
+});
+
+exports.updateStudent = catchAsync(async (req, res, next) => {
+  const student = await Student.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      student: student,
+    },
+  });
+});
+
+exports.deleteStudent = catchAsync(async (req, res, next) => {
+  await Student.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
