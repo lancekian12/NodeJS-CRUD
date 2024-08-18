e1465b54e2b405575b9f9d2a2eb4d47e28c2a4b8
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-08-18 09:14:37 +0800

    modify student schema

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
   studentName: {
@@ -6,19 +6,23 @@ const studentSchema = new mongoose.Schema({
     required: true,
     maxlength: [
       50,
-      "A student name must have less or equal than 50 characters",
+      'A student name must have less or equal than 50 characters',
     ],
     minlength: [
       10,
-      "A student name must have more or equal than 10 characters",
+      'A student name must have more or equal than 10 characters',
     ],
   },
   age: {
     type: Number,
-    required: [true, "A student should have age"],
+    required: [true, 'A student should have age'],
+  },
+  section: {
+    type: String,
+    required: true,
   },
 });
 
-const Student = mongoose.model("Student", studentSchema);
+const Student = mongoose.model('Student', studentSchema);
 
 module.exports = Student;
