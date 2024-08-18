418e9e99594a7ac80e316b08bf5c6698d5b4bd7f
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-08-18 08:35:42 +0800

    Putting comment to identify each uses

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 // const hpp = require("hpp");
 
 const app = express();
 const studentRouter = require("./routes/studentRoute");
 
+// 1. Middlewares
 app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log("Hello from the server 👋");
+  next();
+});
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
   next();
 });
 
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
+
+// Environment Variables
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
+// App.js
 const app = require("./app");
+
+// DB Variable
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
 
+//Function to Connecting DB
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -16,7 +22,10 @@ mongoose
   })
   .then(() => console.log("DB connection successful!"));
 
+// SERVER PORT
 const port = process.env.PORT || 3000;
+
+// Connection
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
