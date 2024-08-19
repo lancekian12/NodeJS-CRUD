801eb33c21e00a6ece705092d89fa89c3d5be8f4
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-08-19 19:49:18 +0800

    Put server.js in app.js

--- app.js
+++ app.js
@@ -1,37 +1,66 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-const cors = require('cors');
-
-// const hpp = require("hpp");
-
-const app = express();
-
-app.use(cors());
-
 const studentRouter = require('./routes/studentRoute');
 
-// 1. Middlewares
+// Load environment variables from the config file
+dotenv.config({ path: './config.env' });
+
+// Create the Express app
+const app = express();
+
+// Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Custom middlewares to log requests and add request time
 app.use((req, res, next) => {
   console.log('Hello from the server 👋');
   next();
 });
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-//3.) ROUTES
+// Routes
 app.use('/api/v1/student', studentRouter);
 
+app.get('/', (req, res) => {
+  res.redirect('/api/v1/student');
+});
+
+// Handle all undefined routes
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
+// Global error handling middleware
 app.use(globalErrorHandler);
 
-// 4.) START SERVER
+// Database connection
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}...`);
+});
+
 module.exports = app;
