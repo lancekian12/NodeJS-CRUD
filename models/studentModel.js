0b45030050e82122496721e79f60dd36cdb3d8b8
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-08-23 22:02:11 +0800

    update dummy data

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,28 +1,52 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  studentName: {
+  firstName: {
     type: String,
-    required: true,
+    required: [true, 'A student should have first name'],
     maxlength: [
       50,
-      'A student name must have less or equal than 50 characters',
+      'A student first name must have less or equal than 50 characters',
     ],
     minlength: [
-      10,
-      'A student name must have more or equal than 10 characters',
+      6,
+      'A student first name must have more or equal than 10 characters',
     ],
   },
-  age: {
-    type: Number,
-    required: [true, 'A student should have age'],
-  },
-  section: {
+  lastName: {
     type: String,
-    required: [true, 'A student should have a section'],
+    required: [true, 'A student should have last name'],
+    maxlength: [
+      50,
+      'A student last name must have less or equal than 50 characters',
+    ],
+    minlength: [
+      6,
+      'A student last name must have more or equal than 10 characters',
+    ],
   },
-  tuitionFee: {
-    type: Number,
+  course: {
+    type: String,
+    required: [true, 'A student should have a course'],
+    maxlength: [
+      50,
+      'A student course must have less or equal than 50 characters',
+    ],
+    minlength: [
+      6,
+      'A student last name must have more or equal than 10 characters',
+    ],
+  },
+  year: {
+    type: String,
+    required: [true, 'A student should have a year'],
+    maxlength: [
+      50,
+      'A student year must have less or equal than 50 characters',
+    ],
+  },
+  enrolled: {
+    type: Boolean,
   },
   createdAt: {
     type: Date,
